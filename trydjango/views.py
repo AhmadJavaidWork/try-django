2d71e11efac7e5fef756a33351074dea94fc4765
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2024-05-17 16:44:54 +0500

    serve articles from db

--- trydjango/views.py
+++ trydjango/views.py
@@ -1,11 +1,17 @@
 import random
 from django.http import HttpResponse
 
+from articles.models import Article
+
 
 def home_view(request):
-    name = "Justin"
-    number = random.randint(10, 12938742)
-    HTML_STRING = f"""
-    <h1>Hello {name} - {number}</h1>
+    random_id = random.randint(1, 4)
+    article_obj = Article.objects.get(id=random_id)
+    H1_STRING = f"""
+    <h1>{article_obj.title} ({article_obj.id})!</h1>
     """
+    P1_STRING = f"""
+    <p>{article_obj.content}!</p>
+    """
+    HTML_STRING = H1_STRING + P1_STRING
     return HttpResponse(HTML_STRING)
