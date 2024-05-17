19815928a9e9d2cd76ff1a042bec0b02196eede9
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2024-05-17 21:33:02 +0500

    create article form

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from .forms import ArticleForm
 from .models import Article
 # Create your views here.
 
@@ -25,17 +26,40 @@ def article_search_view(request):
 
 @login_required
 def article_create_view(request):
+    form = ArticleForm(request.POST or None)
+    context = {
+        "form": form
+    }
 
-    context = {}
-
-    if request.method == 'POST':
-        title = request.POST.get("title")
-        content = request.POST.get("content")
+    if form.is_valid():
+        title = form.cleaned_data.get("title")
+        content = form.cleaned_data.get("content")
         article_object = Article.objects.create(title=title, content=content)
         context['object'] = article_object
         context['created'] = True
 
     return render(request, "articles/create.html", context=context)
+
+
+# @login_required
+# def article_create_view(request):
+#     form = ArticleForm()
+#     context = {
+#         "form": form
+#     }
+
+#     if request.method == 'POST':
+#         form = ArticleForm(request.POST)
+#         context['form'] = form
+#         if form.is_valid():
+#             title = request.POST.get("title")
+#             content = request.POST.get("content")
+#             article_object = Article.objects.create(
+#                 title=title, content=content)
+#             context['object'] = article_object
+#             context['created'] = True
+
+#     return render(request, "articles/create.html", context=context)
 
 
 def article_detail_view(request, id=None):
