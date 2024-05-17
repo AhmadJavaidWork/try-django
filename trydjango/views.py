d691479f72133d989723a00a318afb2ca6387155
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2024-05-17 17:27:37 +0500

    query set

--- trydjango/views.py
+++ trydjango/views.py
@@ -7,11 +7,13 @@ from articles.models import Article
 def home_view(request):
     random_id = random.randint(1, 4)
     article_obj = Article.objects.get(id=random_id)
-
+    article_queryset = Article.objects.all()
     context = {
+        "object_list": article_queryset,
         "id": article_obj.id,
         "title": article_obj.title,
-        "content": article_obj.content
+        "content": article_obj.content,
+        "object": article_obj,
     }
     HTML_STRING = render_to_string("home-view.html", context=context)
     return HttpResponse(HTML_STRING)
