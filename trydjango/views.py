1a01ac122636590e3f2b68aa94ddd385d3b69980
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2024-05-17 17:06:41 +0500

    added template

--- trydjango/views.py
+++ trydjango/views.py
@@ -1,17 +1,17 @@
 import random
 from django.http import HttpResponse
-
+from django.template.loader import render_to_string
 from articles.models import Article
 
 
 def home_view(request):
     random_id = random.randint(1, 4)
     article_obj = Article.objects.get(id=random_id)
-    H1_STRING = f"""
-    <h1>{article_obj.title} ({article_obj.id})!</h1>
-    """
-    P1_STRING = f"""
-    <p>{article_obj.content}!</p>
-    """
-    HTML_STRING = H1_STRING + P1_STRING
+
+    context = {
+        "id": article_obj.id,
+        "title": article_obj.title,
+        "content": article_obj.content
+    }
+    HTML_STRING = render_to_string("home-view.html", context=context)
     return HttpResponse(HTML_STRING)
