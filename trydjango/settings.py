bc2c9e8726bfae51645a231d62781300032e48fa
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2024-05-30 16:32:35 +0500

    added dotenv

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)(@16!^z6&4x_dz#4ml$ef8u6hm&vw+l)w+qpoh@sqkzfk-q!@'
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY', 'django-insecure-)(@16!^z6&4x_dz#4ml$ef8u6hm&vw+l)w+qpoh@sqkzfk-q!@')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG')) == "1"
 
 ALLOWED_HOSTS = []
+if not DEBUG:
+    ALLOWED_HOSTS += [os.environ.get('ALLOWED_HOSTS')]
 
 
 # Application definition
